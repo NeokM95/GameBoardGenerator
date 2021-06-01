1d90ccf52d84634c7e34f3bb83473b166c69d193
NeokM95 <koenm95@outlook.com>
2021-06-01 12:46:00 +0200

    Updated version

--- src/nl/koenm/GameBoardGenerator.java
+++ src/nl/koenm/GameBoardGenerator.java
@@ -14,19 +14,42 @@ public class GameBoardGenerator {
 
         int[] board = new int[boardSize];
 
-        for (int i = 0; i < board.length; i++) {
-            if ((i + 1) % Math.sqrt(board.length) != 0) {
-                System.out.printf("%02d | ", i);
-            } else {
-                System.out.printf("%02d%n", i);
-                if (i < board.length - Math.sqrt(board.length)) {
-                    System.out.print("---+");
-                    for (int j = 0; j < Math.sqrt(board.length) - 2; j++) {
-                        System.out.print("----+");
+        double v = board.length - Math.sqrt(board.length);
+
+        if (boardLength < 11) {
+
+            for (int i = 0; i < board.length; i++) {
+                if ((i + 1) % Math.sqrt(board.length) != 0) {
+                    System.out.printf("%02d | ", i);
+                } else {
+                    System.out.printf("%02d%n", i);
+                    if (i < v) {
+                        System.out.print("---+");
+                        for (int j = 0; j < Math.sqrt(board.length) - 2; j++) {
+                            System.out.print("----+");
+                        }
+                        System.out.println("---");
                     }
-                    System.out.println("---");
                 }
             }
+
+        } else {
+
+            for (int i = 0; i < board.length; i++) {
+                if ((i + 1) % Math.sqrt(board.length) != 0) {
+                    System.out.printf("%03d | ", i);
+                } else {
+                    System.out.printf("%03d%n", i);
+                    if (i < v) {
+                        System.out.print("----+");
+                        for (int j = 0; j < Math.sqrt(board.length) - 2; j++) {
+                            System.out.print("-----+");
+                        }
+                        System.out.println("----");
+                    }
+                }
+            }
+
         }
 
     }
